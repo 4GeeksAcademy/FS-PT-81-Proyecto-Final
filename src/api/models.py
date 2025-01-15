932603682292fddcaed57cadefd15da4041ad857
antomanu95 <antoniomr.estevez@hotmail.com>
2025-01-15 11:08:14 +0000

    First

--- src/api/models.py
+++ src/api/models.py
@@ -1,12 +1,20 @@
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy import ForeignKey
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    profile_picture = db.Column(db.String(50), nullable=False)
+    
+    comments = db.relationship("Comments", backref='user', lazy=True)
+    posts = db.relationship("Post", backref='user', lazy=True)
+    favourites = db.relationship("Favourites", backref='user', lazy=True)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -14,6 +22,128 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
+            "name": self.name,
+            "last_name": self.last_name,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "profile_picture": self.profile_picture
         }
+
+class Post(db.Model):
+    __tablename__ = 'post'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, ForeignKey('user.id'))
+    title = db.Column(db.String(80), nullable=False)
+    body = db.Column(db.String(), nullable=False)
+    image = db.Column(db.String(800), nullable=False)
+    created_at = db.Column(db.String(50), nullable=False)
+    category_id = db.Column(db.Integer, ForeignKey('category.id'))
+    
+    comments = db.relationship("Comments", backref='post', lazy=True)
+    favourites = db.relationship("Favourites", backref='post', lazy=True)
+
+    def __repr__(self):
+        return f'<Post {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "title": self.title,
+            "body": self.body,
+            "image": self.image,
+            "created_at": self.created_at,
+            "category_id": self.category_id
+        }
+
+class Comments(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, ForeignKey('user.id'))
+    post_id = db.Column(db.Integer, ForeignKey('post.id'))
+    comment_text = db.Column(db.String(500), nullable=False)
+
+    def __repr__(self):
+        return f'<Comments {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "post_id": self.post_id,
+            "comment_text": self.comment_text
+        }
+
+class Favourites(db.Model):
+    __tablename__ = 'favourites'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, ForeignKey('user.id'))
+    post_id = db.Column(db.Integer, ForeignKey('post.id'))
+
+    def __repr__(self):
+        return f'<Favourites {self.id}>'
+
+    def serialize(self):
+        return {
+            "favourites_id": self.id,
+            "user_id": self.user_id,
+            "post_id": self.post_id
+        }
+
+class POI(db.Model):
+    __tablename__ = 'poi'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    address = db.Column(db.String(250), nullable=False)
+    city = db.Column(db.String(250), nullable=False)
+    continent = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    pictures = db.Column(db.String(800), nullable=False)
+    category_id = db.Column(db.Integer, ForeignKey('category.id'))
+
+    def __repr__(self):
+        return f'<POI {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "address": self.address,
+            "city": self.city,
+            "continent": self.continent,
+            "description": self.description,
+            "pictures": self.pictures,
+            "category_id": self.category_id
+        }
+
+class Category(db.Model):
+    __tablename__ = 'category'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    posts = db.relationship("Post", backref='category', lazy=True)
+    pois = db.relationship("POI", backref='category', lazy=True)
+
+    def __repr__(self):
+        return f'<Category {self.name}>'
+    
+    def serialice(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "posts": self.posts,
+            "pois": self.pois
+        }
+
+# comments: [comment.serialize() for comment in self.comments]
+    
+
+
+
+
+
+
+
+
+    
+
+    
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Favourites, POI, Post, Comments, Category
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -12,11 +12,24 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+# Metodo GET #
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+# Metodo POST #
 
-    return jsonify(response_body), 200
+@app.route('/favourites/POI/<int:poi_id>', methods=['POST'])
+def handle_fav_poi(poi_id):
+    user_id = 1
+
+    existing_favourites = Favourites_Poi.query.filter_by(user_id = user_id, poi_id = poi_id).first()
+    if existing_favourites:
+        return jsonify({'error': 'planet already in favourites'}), 400
+    
+
+    new_favourite_poi = Favourites_Poi(user_id = user_id, poi_id_id = poi_id)
+    db.session.add(new_favourite_poi)
+    db.session.commit()
+
+    return jsonify({"msg": "Planet added to favourites"})
+
+
+

c7c1a912341e3b8479720bfd58ae433f14498c2b
Yabel90 <yasminabelhaj@hotmail.com>
2025-01-16 21:26:43 +0000

    chore registro

---
+++ src/front/js/component/formularioRegistro.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {useContext, useState  } from "react";
+import { Context } from "../store/appContext";
+
+
+
+export const Registro = () => {
+    const { store, actions } = useContext(Context);
+    const[form, setForm] = useState ({
+        email: '', 
+        contraseña: ''
+    });
+    const handleSubmit = e => {
+		e.preventDefault();
+		actions.registro(form);
+
+	};
+    return(
+        <form onSubmit={handleSubmit}>
+            <h2>Registro</h2>
+            <input
+            type="email"
+            placeholder="Email"
+            value={form.email}
+            onChange={(e) => setForm ({ ...form, email: e.target.value})}
+            />
+            <input
+            type="contraseña"
+            placeholder="Contraseña"
+            value={form.contraseña}
+            onChange={(e) => setForm({ ...form, contraseña: e.target.value})}
+            />
+            <button type="submit">Registrarse</button>
+        </form>
+    )
+}

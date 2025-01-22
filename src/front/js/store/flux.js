85fcd9bad3a97dcc631a330bcb9b6f38f5096656
Yabel90 <yasminabelhaj@hotmail.com>
2025-01-22 11:41:47 +0000

    añadiendo direccion de api

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,41 +2,53 @@ const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 			perfilUsuario: null,
-			url:"",
+			post: [],
+			url: "https://legendary-giggle-69rj79g5qgj4fw56-3001.app.github.dev/",
 		},
 		actions: {
-		getPerfilUsuario: async () => {
-			try {
-				console.log("obteniendo el usuario");
-				const response = await fetch(`${getStore().url}/perfil/usuario`, {
-					method: 'GET',
-					headers: {
-						'Content-Type': 'application/json'
-					}
-				});
-				if (!response.ok) throw new Error('Error obteniendo usuario');
-				const data = await response.json();
-				setStore({ perfilUsuario: data});
-			} catch (error) {
-				console.error("error obteniendo el usuario", error);
-			}
-		},
-		changeColor: (index, color) => {
-			//get the store
-			const store = getStore();
+			getPerfilUsuario: async () => {
+				try {
+					console.log("obteniendo el usuario");
+					const response = await fetch(`${getStore().url}users/<int:id>`, {
+						method: 'GET',
+						headers: {
+							'Content-Type': 'application/json'
+						}
+					});
+					if (!response.ok) throw new Error('Error obteniendo usuario');
+					const data = await response.json();
+					setStore({ perfilUsuario: data });
+				} catch (error) {
+					console.error("error obteniendo el usuario", error);
+				}
+			},
+			getPost: async () => {
+				//get the stor
+				try {
+					const respone = await fetch(`${getStore().url}/post`, {
+						method: "GET",
+						headers: {
+							"Content-Type": "application/json",
+						},
+					});
+					if (!response.ok) throw new Error("Error obteniendo los posts")
 
-			//we have to loop the entire demo array to look for the respective index
-			//and change its color
-			const demo = store.demo.map((elm, i) => {
-				if (i === index) elm.background = color;
-				return elm;
-			});
-
+					//we have to loop the entire demo array to look for the respective index
+					//and change its color
+					const data = await response.json();
+					setStore({ post: data });
+					console.log("post cargado", data);
+				} catch (error) {
+					console.error("Error cargando post:", error);
+				}
+			},
 			//reset the global store
-			setStore({ demo: demo });
-		}
-	}
-};
+			resetDemo: () => {
+				setStore({ demo: demo });
+			},
+
+		},
+	};
 };
 
 export default getState;

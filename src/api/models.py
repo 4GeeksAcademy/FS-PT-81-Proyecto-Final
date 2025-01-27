655f7e00cce19145e2cafc7ca2d2311e7a924f0b
antomanu95 <antoniomr.estevez@hotmail.com>
2025-01-27 11:30:49 +0000

    Actualizar rutas favoritos

--- src/api/models.py
+++ src/api/models.py
@@ -2,49 +2,41 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import ForeignKey
 db = SQLAlchemy()
 
-class User(db.Model):
+class Users(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
-    name = db.Column(db.String(50), nullable=False)
-    last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
-    profile_picture = db.Column(db.String(50), nullable=False)
-    
+    #profile_picture = db.Column(db.String(50), nullable=True)
     comments = db.relationship("Comments", backref='user', lazy=True)
-    posts = db.relationship("Post", backref='user', lazy=True)
+    posts = db.relationship("Posts", backref='user', lazy=True)
     favourites = db.relationship("Favourites", backref='user', lazy=True)
 
     def __repr__(self):
         return f'<User {self.email}>'
+    
+def serialize(self):
+    return {
+        "id": self.id,
+        "username": self.username,
+        "email": self.email,
+    }
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "username": self.username,
-            "name": self.name,
-            "last_name": self.last_name,
-            "email": self.email,
-            "profile_picture": self.profile_picture
-        }
-
-class Post(db.Model):
+class Posts(db.Model):
     __tablename__ = 'post'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, ForeignKey('user.id'))
     title = db.Column(db.String(80), nullable=False)
     body = db.Column(db.String(), nullable=False)
     image = db.Column(db.String(800), nullable=False)
-    created_at = db.Column(db.String(50), nullable=False)
-    category_id = db.Column(db.Integer, ForeignKey('category.id'))
-    
+    #created_at = db.Column(db.String(50), nullable=False)
     comments = db.relationship("Comments", backref='post', lazy=True)
     favourites = db.relationship("Favourites", backref='post', lazy=True)
 
     def __repr__(self):
-        return f'<Post {self.id}>'
-
+        return f'<Posts {self.id}>'
+    
     def serialize(self):
         return {
             "id": self.id,
@@ -55,9 +47,9 @@ class Post(db.Model):
             "created_at": self.created_at,
             "category_id": self.category_id
         }
-
+    
 class Comments(db.Model):
-    __tablename__ = 'comments'
+    __tablename__ = 'comment'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, ForeignKey('user.id'))
     post_id = db.Column(db.Integer, ForeignKey('post.id'))
@@ -65,7 +57,7 @@ class Comments(db.Model):
 
     def __repr__(self):
         return f'<Comments {self.id}>'
-
+    
     def serialize(self):
         return {
             "id": self.id,
@@ -73,70 +65,22 @@ class Comments(db.Model):
             "post_id": self.post_id,
             "comment_text": self.comment_text
         }
-
+    
 class Favourites(db.Model):
-    __tablename__ = 'favourites'
+    __tablename__ = 'favourite'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, ForeignKey('user.id'))
     post_id = db.Column(db.Integer, ForeignKey('post.id'))
 
     def __repr__(self):
         return f'<Favourites {self.id}>'
-
+    
     def serialize(self):
         return {
             "favourites_id": self.id,
             "user_id": self.user_id,
             "post_id": self.post_id
         }
-
-class POI(db.Model):
-    __tablename__ = 'poi'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(250), nullable=False)
-    address = db.Column(db.String(250), nullable=False)
-    city = db.Column(db.String(250), nullable=False)
-    continent = db.Column(db.String(50), nullable=False)
-    description = db.Column(db.Text, nullable=False)
-    pictures = db.Column(db.String(800), nullable=False)
-    category_id = db.Column(db.Integer, ForeignKey('category.id'))
-
-    def __repr__(self):
-        return f'<POI {self.id}>'
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "address": self.address,
-            "city": self.city,
-            "continent": self.continent,
-            "description": self.description,
-            "pictures": self.pictures,
-            "category_id": self.category_id
-        }
-
-class Category(db.Model):
-    __tablename__ = 'category'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(250), nullable=False)
-    posts = db.relationship("Post", backref='category', lazy=True)
-    pois = db.relationship("POI", backref='category', lazy=True)
-
-    def __repr__(self):
-        return f'<Category {self.name}>'
-    
-    def serialice(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "posts": self.posts,
-            "pois": self.pois
-        }
-
-# comments: [comment.serialize() for comment in self.comments]
-    
-
 
 
 

ea750c3a60e4c82cb9504ea2a5f3007e097a106a
Cadivals <4geeksalex@gmail.com>
2025-02-06 18:33:13 +0000

    feat:vista quienes somos

--- src/front/js/pages/quienesSomos.js
+++ src/front/js/pages/quienesSomos.js
@@ -1,34 +1,87 @@
 import React from "react";
 
-
-import '../../styles/destinos_ciudades.css';
-
 export const QuienesSomos = () => {
-    return (
-        <div className="container">
-            <img src="" alt="Imagen de ejemplo" />
-            <p className="m-auto">Hola</p>
-            <div id="carouselExampleRide" className="carousel slide" data-bs-ride="true">
-                <div className="carousel-inner">
-                    <div className="carousel-item active" data-bs-interval="5000">
-                        <p>SOBRE NOSOTROS</p>
-                    </div>
-                    <div className="carousel-item" data-bs-interval="5000">
-                        <p>SOBRE NOSOTROS</p>
-                    </div>
-                    <div className="carousel-item" data-bs-interval="5000">
-                        <p>SOBRE NOSOTROS</p>
-                    </div>
-                </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Previous</span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Next</span>
-                </button>
-            </div>
+  return (
+    <div className="container">
+      <h4 className="city_destinos text-center mb-5">
+        <span>¿QUIÉNES SOMOS?</span>
+      </h4>
+
+      <div className="row align-items-center">
+      
+        <div className="col-lg-9 col-md-8 col-sm-12 text-center text-md-start">
+          <p>
+            ¡Hola, os doy la bienvenida a nuestra página web!  
+            <br />
+            Soy <b>Yasmina</b>, una persona apasionada por la tecnología y los retos de la programación. Me encanta explorar nuevas soluciones y aprender constantemente para mejorar mis habilidades. 
+            Además, disfruto viajar y descubrir nuevos lugares, especialmente aquellos cerca del mar, donde encuentro inspiración y tranquilidad. 
+            Siempre estoy en busca de nuevos desafíos que me permitan crecer tanto profesional como personalmente.
+          </p>
         </div>
-    );
+
+      
+        <div className="col-lg-3 col-md-4 col-sm-12 text-center">
+          <img className="img-fluid rounded-circle" style={{ maxWidth: "150px" }}
+            src="https://static.vecteezy.com/system/resources/previews/003/687/259/non_2x/muslim-girl-wearing-chador-free-vector.jpg"
+            alt="perfil Yasmina"
+            
+          />
+        </div>
+
+        <div className="col-lg-9 col-md-8 col-sm-12 text-center text-md-start">
+          <p>
+            ¡Hola soy <b>Antonio </b>, me encantan los animales y estar al aire libre en contacto con la naturaleza.
+            Me apasiona viajar y conocer todos los rincones que atesora este planeta, concretamente los que están en el fondo del mary ver todo tipo de criaturas, es un mundo alucinante.
+            No me gusta mucho los estilos por lo que soy miembro del team Backend.
+          </p>
+        </div>
+
+       
+        <div className="col-lg-3 col-md-4 col-sm-12 text-center">
+          <img className="img-fluid rounded-circle" style={{ maxWidth: "150px" }}
+            src="https://miro.medium.com/max/2400/1*c_wXVy04pUp8c9STWp4uOg.png"
+            alt="perfil Antonio"
+            
+          />
+        </div>
+       
+
+        <div className="col-lg-9 col-md-8 col-sm-12 text-center text-md-start">
+          <p>
+            ¡Hola soy <b>José Luis</b>. Me considero un explorador del mundo. Me gusta la fotografía, captar un poquito de cada ciudad a la que voy 
+            y disfrutar de la gastronomía de todos esos sitios.
+            También me gusta hacer deporte al aire libre y así conocer un poco más del mundo y sus maravillas.
+          </p>
+        </div>
+
+       
+        <div className="col-lg-3 col-md-4 col-sm-12 text-center">
+          <img className="img-fluid rounded-circle" style={{ maxWidth: "150px" }}
+            src="https://img.freepik.com/vector-premium/personaje-nino-sonriente-esta-disenado-utilizando-estilo-diseno-plano-simple_995281-466.jpg"
+            alt="perfil José Luis"
+            
+          />
+        </div>
+
+        <div className="col-lg-9 col-md-8 col-sm-12 text-center text-md-start">
+          <p>
+            ¡Hola soy <b>Alexis</b>. Soy un apasionado de la naturalezaa y los animales, pero mi debilidad son los perretes y su alma tan pura.
+            Me encanta viajar, siempre que puedo, y explorar nuevos lugares y sus culturas. Creo que la mejor forma de conocer a los demás y ver que 
+            no somos los únicos es viajando y compartiendo con los demás. Me encanta el mar y la tranquilidad que se encuentra en él. Hace un par 
+            de años tuve la oportunidad de practicar buceo en Kho Tao (Tailandia) y quedé enamorado de la profundidad marina. Espero que en esta web
+            encuentres todo lo que buscas para tu próxima aventura.
+          </p>
+        </div>
+
+     
+        <div className="col-lg-3 col-md-4 col-sm-12 text-center">
+          <img className="img-fluid rounded-circle" style={{ maxWidth: "150px" }}
+            src="https://visivalab.com/wp-content/uploads/2024/05/Nosotros-17.jpg"
+            alt="perfil Alexis"
+            
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
